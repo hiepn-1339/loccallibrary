a7141e7802abd23a34328b45505271629260e9bf
hiepnn-1339 <nguyen.ngoc.hiep@sun-asterisk.com>
2024-04-09 17:11:13 +0700

    Part 4: Template primer

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,31 @@ import morgan from 'morgan';
 import route from './routes';
 import 'reflect-metadata';
 import { AppDataSource } from './data-source';
+import i18next from 'i18next';
+import Backend from 'i18next-node-fs-backend';
+import i18nextMiddleware from 'i18next-http-middleware';
 
 const app: Express = express();
+i18next
+  .use(Backend)
+  .use(i18nextMiddleware.LanguageDetector)
+  .init({
+    backend: {
+      loadPath: __dirname + '/locales/{{lng}}/{{lng}}.json',
+    },
+    detection: {
+      order: ['querystring', 'cookie'],
+      caches: ['cookie'],
+    },
+    fallbackLng: 'vi',
+    preload: ['vi', 'en'],
+  });
+app.use(i18nextMiddleware.handle(i18next));
+app.use(
+  i18nextMiddleware.handle(i18next, {
+    removeLngFromUrl: false,
+  }),
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,11 +42,12 @@ app.set('view engine', 'pug');
 
 app.use('', route);
 
-app.use((err: Error, req: Request, res: Response) => {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(500);
-  res.render('error');
+app.use((err: any, req: Request, res: Response) => {
+  if (err) {
+    console.error(err.stack);
+  } else {
+    res.status(500).send('Đã xảy ra lỗi!');
+  }
 });
 
 const port = config.port;
